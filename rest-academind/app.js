23c4f5624173b68b691931abb9ff67956e5ff38b
KRxSH <61300993+KRxSH@users.noreply.github.com>
2020-08-14 10:10:26 +0530

    Error: Cannot find module './api/routes/products'
    at Object.<anonymous> (I:\GitHub\Practice\Web-Dev-Practice\rest-academind\app.js:4:23)

---
+++ rest-academind/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app =  express();
+
+const productRoutes = require('./api/routes/products');
+
+app.use('/products'/*filter*/, productRoutes/*handler*/); 
+//sets up a middleware
+//forward all requests through "/products" (i.e., URL request) to ./api/routes/products(.js) code
+
+module.exports = app; // works with: const app = require('./app');
