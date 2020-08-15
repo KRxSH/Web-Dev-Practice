458d39b7415ad4a2bc1123fa06263682fdaff314
Thakur <61300993+thakur-patel@users.noreply.github.com>
2020-08-15 16:59:45 +0530

    Delete server.js

--- rest-academind/server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http'); //use of this???
-const app = require('./app');
-
-const port = process.env.PORT || 3000;
-
-const server = http.createServer(app); //use of this???
-
-server.listen(port, () => console.log(`Listening on Port ${port}`));
